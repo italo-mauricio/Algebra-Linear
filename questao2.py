9597afd3b0cfbaa0e2bf91647a9eb207bd6640c2
italo <italogithub@gmail.com>
2023-05-09 22:27:12 -0300

    refatoring

--- questao2.py
+++ questao2.py
@@ -1,24 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-velocidade = [0, 2, 4, 6, 8, 10]
-forca = [0, 2.90, 14.8, 39.6, 74.3, 119]
+velocidade = np.array([0, 2, 4, 6, 8, 10])
+forca = np.array([0, 2.90, 14.8, 39.6, 74.3, 119])
 
 coeficientes = np.polyfit(velocidade, forca, 5)
 polinomial = np.poly1d(coeficientes)
 
-velocidade_estimada = 750
+velocidade_estimada = 7.5
 forca_estimada = polinomial(velocidade_estimada)
 
-print("O polinômio interpolador é: ")
-print(f"p(t) = {coeficientes[0]:.4f} + {coeficientes[1]:.4f}t^1 + {coeficientes[2]:.4f}t^2 + {coeficientes[3]:.4f}t^3 + {coeficientes[4]:.4f}t^4 + {coeficientes[5]:.4f}t^5")
+print("O polinômio interpolador é:")
+print(f"p(t) = {coeficientes[0]:.4f} + {coeficientes[1]:.4f}t + {coeficientes[2]:.4f}t^2 + {coeficientes[3]:.4f}t^3 + {coeficientes[4]:.4f}t^4 + {coeficientes[5]:.4f}t^5")
 
-print(f"A força estimada na velocidade de {velocidade_estimada} ft/sec é de 16.91 lb.")
+print(f"A força estimada na velocidade de {velocidade_estimada} ft/sec é de {forca_estimada:.2f} lb.")
 
 plt.scatter(velocidade, forca, color='red', label='Dados')
 plt.plot(velocidade, polinomial(velocidade), label='Polinômio Interpolador')
-plt.xlabel('Velocidade (100 ft/sec)')
-plt.ylabel('Força (100 lb)')
+plt.xlabel('Velocidade (ft/sec)')
+plt.ylabel('Força (lb)')
 plt.title('Dados e Polinômio Interpolador')
 plt.legend()
 plt.grid(True)
